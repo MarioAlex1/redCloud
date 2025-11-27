ffd8bc93293f315e4ec97b7188085b9d3fa8f1c2
Mario Alex <alexxbarbosa662@gmail.com>
2025-11-26 21:19:35 -0300

    chore: resolving typescript typing issues

---
+++ redCloud/declarations.d.ts
@@ -0,0 +1,4 @@
+declare module '@expo/vector-icons'
+declare module '@expo/vector-icons/FontAwesome5'
+
+//pra resolver a intriga do typescript com o modulo que ele insistia em dizer que nao existia
