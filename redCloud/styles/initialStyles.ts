5112a89c5528ab916d42e2e53814bd8f1780ca71
Mario Alex <alexxbarbosa662@gmail.com>
2025-11-26 21:21:45 -0300

    chore: add coments

---
+++ redCloud/styles/initialStyles.ts
@@ -0,0 +1,53 @@
+import { StyleSheet } from "react-native";
+
+export const initialStyles = StyleSheet.create({
+    background: {
+        flex: 1, //faz a imagem aparecer na tela
+    },
+
+    skipContainer: { //container do skip
+        position: "absolute",
+        top: "10%",
+        right: "10%",
+    },
+
+    skipText: {
+        color: "#ffff",
+    },
+
+    buttonsContainer: { //isso controla o posicionamento dos botões
+        position: "absolute",
+        bottom: "10%",
+        width: "100%",
+        alignItems: "center",
+        gap: 15,
+    },
+
+    buttonLogin: { //esse e o de baixo controla a estetica dos botões
+        width: "65%",
+        padding: 10,
+        borderRadius: 30,
+        backgroundColor: "rgba(255, 26, 26, 0.59)",
+        alignItems: "center",
+    },
+
+    buttonRegister: {
+        width: "65%",
+        padding: 10,
+        borderRadius: 30,
+        backgroundColor: "rgba(255, 255, 255, 0.7)",
+        alignItems: "center",
+    },
+
+    buttonText: {
+        fontSize: 18,
+        fontWeight: "bold",
+        color: "#ffff",
+    },
+
+    buttonTextRegister: {
+        fontSize: 18,
+        fontWeight: "bold",
+        color: "#0000",
+    }
+});
