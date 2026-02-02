f579e2dbf9d474a58fe523e8f535aed6d101ae0c
Mario Alex <alexxbarbosa662@gmail.com>
2026-02-02 08:55:56 -0300

    feat: implement logout functionality with error handling

--- redCloud/app/user/UserLogout.tsx
+++ redCloud/app/user/UserLogout.tsx
@@ -1,10 +1,27 @@
-import { TouchableOpacity, Text } from "react-native";
+import { TouchableOpacity, Text, Alert } from "react-native";
+import { router } from "expo-router";
 import { UserStyles } from "../../src/styles/user.styles";
 
+import * as authService from "../../src/services/authServices";
+import * as authStorage from "../../src/storage/authStorage";
+
 export default function UserLogout() {
-    return (
-        <TouchableOpacity style={UserStyles.logoutButton}>
-            <Text style={UserStyles.logoutButtonText}>Sair</Text>
-        </TouchableOpacity>
-    );
+  const handleLogout = async () => {
+    try {
+      await authService.logout();      // Firebase Auth
+      await authStorage.removeUser();  // AsyncStorage local
+      router.replace("/login");        // Volta para login
+    } catch {
+      Alert.alert("Erro", "Falha ao sair da conta");
+    }
+  };
+
+  return (
+    <TouchableOpacity
+      style={UserStyles.logoutButton}
+      onPress={handleLogout}
+    >
+      <Text style={UserStyles.logoutButtonText}>Sair</Text>
+    </TouchableOpacity>
+  );
 }
