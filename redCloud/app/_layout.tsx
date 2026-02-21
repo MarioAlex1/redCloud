cccd600b75126e7ab0d9752ba881f28147aa8c64
Mario Alex <alexxbarbosa662@gmail.com>
2026-02-21 17:27:35 -0300

    refactor: wrap Layout component with ThemeProvider for dynamic theming support

--- redCloud/app/_layout.tsx
+++ redCloud/app/_layout.tsx
@@ -1,13 +1,16 @@
 import { Stack } from "expo-router";
+import { ThemeProvider } from "../src/theme/ThemeContext";
 
 export default function Layout() {
     return (
-        <Stack
-            screenOptions={{
-                headerTransparent: true,
-                headerTitle: "",
-                headerTintColor: "#ffff"
-            }}
-        />
+        <ThemeProvider>
+            <Stack
+                screenOptions={{
+                    headerTransparent: true,
+                    headerTitle: "",
+                    headerTintColor: "#ffff",
+                }}
+            />
+        </ThemeProvider>
     );
 }
