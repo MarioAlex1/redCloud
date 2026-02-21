e744e335f03f8b1f992d82d7a1eb2a35d385ea3a
Mario Alex <alexxbarbosa662@gmail.com>
2026-02-21 18:24:37 -0300

    refactor: remove Plan components and related logic

--- redCloud/app/plan/index.tsx
+++ redCloud/app/plan/index.tsx
@@ -1,27 +1,2 @@
-import { View, ScrollView } from "react-native";
-import { Stack } from "expo-router";
-import { useTheme } from "../../src/theme/ThemeContext";
-import { useMemo } from "react";
-
-import PlanHeader from "./PlanHeader";
-import PlanBanner from "./PlanBanner";
-import PlanBenefits from "./PlanBenefits";
-import PlanSubscribeButton from "./PlanSubscribeButton";
-
-export default function PlanPage() {
-    const { colors } = useTheme();
-    return (
-        <>
-            <Stack.Screen options={{ headerShown: true }} />
-
-            <View style={{ flex: 1, backgroundColor: colors.bg }}>
-                <ScrollView contentContainerStyle={{ paddingBottom: 120 }}>
-                    <PlanHeader />
-                    <PlanBanner />
-                    <PlanBenefits />
-                    <PlanSubscribeButton />
-                </ScrollView>
-            </View>
-        </>
-    );
-}
+import PlanScreen from "../../src/features/payment/screens/PlanScreen";
+export default PlanScreen;
