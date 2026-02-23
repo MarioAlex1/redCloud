a34cac6e5c7392618ad3036ccb75482fde241984
Mario Alex <alexxbarbosa662@gmail.com>
2026-02-22 23:38:32 -0300

    feat(firebase): update Firebase configuration to use environment variables

--- redCloud/src/services/firebase.ts
+++ redCloud/src/services/firebase.ts
@@ -1,16 +1,11 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA7r-M8Le52VNesV6x5SN51c-51dEswA4o",
-  authDomain: "redcloud-b8eb8.firebaseapp.com",
-  projectId: "redcloud-b8eb8",
-  storageBucket: "redcloud-b8eb8.firebasestorage.app",
-  messagingSenderId: "872888485741",
-  appId: "1:872888485741:web:46b8cddfdd6f69b805fc64"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
 export default firebaseConfig;
